320758ac44a9fe6a1b940fbf3af2d2da469c19ce
naginenihari <naginenihari@gmail.com>
2025-09-28 10:25:03 +0530

    log delete.sh script updated

--- 1-logdelete.sh
+++ 1-logdelete.sh
@@ -24,13 +24,13 @@ if [ ! -d $SOURCE_DIR ]; then
 echo "source directory is not available"
 exit 1
 fi
-#find the 14 days old logs and pass out put to other command in put
+#find the 14 days old logs and pass out put to other command in put##
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -name '*.log' -mtime +10)
+FILES_TO_DELETE=$( find $SOURCE_DIR -type f -name '*.log' -mtime +10 ) 
 
-#once we found the file then delete the file using while condition
+#once we found the file then delete the file using while condition##
  
-while IFS=read -r oldfiles #reading the files line by line
+while IFS=read -r oldfiles #reading the files line by line##
 do
 echo "deleting the $oldfiles" 
 rm -rf $oldfiles
