66ea64eb43af15471d72b72029d851e29c5ce1a5
naginenihari <naginenihari@gmail.com>
2025-09-28 10:37:45 +0530

    log delete.sh script updated

--- 1-logdelete.sh
+++ 1-logdelete.sh
@@ -7,22 +7,27 @@ Y="\e[33m"
 N="\e[37m"
 
 echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
 ## Check the root access or not##
 if [ $USERID -ne 0 ]; then
 echo "Error:you don't have root priveleages"
 exit 1
 fi
+
 #define source directory path##
 SOURCE_DIR=/home/ec2-user/app_logs
+
 #check source directory exist or not#
 if [ ! -d $SOURCE_DIR ]; then
 echo "source directory is not available"
 exit 1
 fi
+
 #find the 14 days old logs and pass out put to other command in put##
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -name '*.log' -mtime +10) 
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -name '*.log' -mtime +14) 
+
 #once we found the file then delete the file using while condition##
-while IFS=read -s oldfiles #reading the files line by line##
+while IFS= read -s oldfiles #reading the files line by line##
 do
 echo "deleting the $oldfiles" 
 rm -rf $oldfiles
